8f588746e02c4ed18306bae548f01e09b41a3055
Jim Tryon <jtryon@mail.greenriver.edu>
2015-03-28 11:02:11 -0700

    Added mongo db

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var router = require('./server/router');
 var morgan = require('morgan');
- 
+
 app.use(bodyParser.urlencoded({extended: true}));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(morgan('dev', { format: 'dev', immediate: true }));
 app.use(router());
- 
-http.createServer(app).listen(9999, function() {
+
+var mongoload = require('./server/mongoload');
+mongoload.load(function(err) {
+  if (err) {
+    console.log('error loading data into mongo');
+    return;
+  }
+  http.createServer(app).listen(9999, function() {
     console.log('Server up: http://localhost:' + 9999);
+  });
 });
+
+
+
